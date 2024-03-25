dcc7463756ab0433d545a837ee9be34c50e839ae
Bobby Stoynov <bobbystoynov@gmail.com>
2024-03-25 16:56:45 -0700

    correct spelling lol

--- solution/src/App.js
+++ solution/src/App.js
@@ -7,12 +7,12 @@ import { getLocations, isNameValid } from "./mock-api/apis";
 import { debounce } from "lodash";
 
 function App() {
-  // In terms of customer experience and efficiency, I have used semantic HTML elements ensuring that:
+  // In terms of customer experience and efficiency, I have used semantic HTML elements to ensure that:
   // The whole interface can be navigated via keyboard allowing for faster interaction with the UI
   // The whole interface is announced correctly by screen readers
-  // Additionally, if we want to avoid layout shift we can use absolute positions for certain elements such as the table and invalind name message
-  // I didn't have enought time to implement responsive styling but I would do it with media queries
-  // I would position the table Name and Location columns one below another and I would make the width of the Name and Location input elements be relative to the current screen as opposed to fixed width
+  // Additionally, if we want to avoid layout shift we can use absolute positions for certain elements such as the table and invalid name Message
+  // I didn't have enough time to implement responsive styling but I would do it with media queries
+  // I would position the table Name and Location columns one below another and I would make the width of the Name and Location input elements be relative to the current screen as opposed to a fixed width
   const [locationList, setLocationList] = useState(null);
   const [name, setName] = useState("");
   const [isValidName, setIsValidName] = useState(true);
